7768a00a2c01be4ee4a00e2504e14602fba9e586
vodjanoj <vladimirs.vodjanojs@gmail.com>
2023-02-03 19:23:04 +0200

    Changed layout record in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Layout from "./components/Layout/Layout";
+import Layout from "./components/layout/Layout";
 import Category from "./components/Products/Category";
 import Cart from "./components/Cart/Cart";
 import ProductDetails from "./components/Products/ProductDetail";
